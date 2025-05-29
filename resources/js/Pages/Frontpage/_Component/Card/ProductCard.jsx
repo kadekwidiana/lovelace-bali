a1519284e1739c8ae52d0ca13d04c8b8c50d64b8
Kadek Widiana <kdkonos10@gmail.com>
2025-05-30 00:10:06 +0800

    fix(product): view product card

--- resources/js/Pages/Frontpage/_Component/Card/ProductCard.jsx
+++ resources/js/Pages/Frontpage/_Component/Card/ProductCard.jsx
@@ -12,54 +12,58 @@ export default function ProductCard({ product, isPromo = false }) {
 
     return (
         <Card className="max-w-sm" imgAlt={product.name} imgSrc={product.image}>
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900">
-                {product.name}
-            </h5>
+            <div className="h-full flex flex-col justify-end gap-2">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900">
+                    {product.name}
+                </h5>
 
-            <div className="flex justify-between items-center">
-                <div className="px-2 text-sm py-1 text-gray-800 rounded-full border border-gray-400">
-                    <span>{product.category.name}</span>
+                <div className="flex justify-between items-center">
+                    <div className="px-2 text-sm py-1 text-gray-800 rounded-full border border-gray-400">
+                        <span>{product.category.name}</span>
+                    </div>
+                    <span className="text-sm text-gray-600">
+                        {product.stock} tersedia
+                    </span>
                 </div>
-                <span className="text-sm text-gray-600">
-                    {product.stock} tersedia
-                </span>
-            </div>
 
-            <div className="flex flex-col gap-1">
-                {isPromo && (
-                    <span className="text-sm text-gray-500 line-through">
-                        {formatRupiah(product.original_price)}
-                    </span>
-                )}
-                <div className="flex items-center justify-between">
-                    <span className="text-lg font-bold text-gray-900">
-                        {formatRupiah(
-                            isPromo ? product.discounted_price : product.price
-                        )}
-                    </span>
-                    <div className="flex items-center justify-end gap-2">
-                        <DetailProductModal
-                            product={product}
-                            trigger={
-                                <FaInfoCircle className="size-6 text-blue-500" />
-                            }
-                            isPromo={isPromo}
-                        />
-                        {!isPromo && (
-                            <Button
-                                size="xs"
-                                onClick={() =>
-                                    handleAddToCart({
-                                        user_id: auth.user.id,
-                                        product_id: product.id,
-                                        quantity: 1,
-                                    })
+                <div className="flex flex-col gap-1">
+                    {isPromo && (
+                        <span className="text-sm text-gray-500 line-through">
+                            {formatRupiah(product.original_price)}
+                        </span>
+                    )}
+                    <div className="flex items-center justify-between">
+                        <span className="text-lg font-bold text-gray-900">
+                            {formatRupiah(
+                                isPromo
+                                    ? product.discounted_price
+                                    : product.price
+                            )}
+                        </span>
+                        <div className="flex items-center justify-end gap-2">
+                            <DetailProductModal
+                                product={product}
+                                trigger={
+                                    <FaInfoCircle className="size-6 text-blue-500" />
                                 }
-                                disabled={product.stock === 0 || isLoading}
-                            >
-                                <HiShoppingCart className="size-4" />
-                            </Button>
-                        )}
+                                isPromo={isPromo}
+                            />
+                            {!isPromo && (
+                                <Button
+                                    size="xs"
+                                    onClick={() =>
+                                        handleAddToCart({
+                                            user_id: auth.user.id,
+                                            product_id: product.id,
+                                            quantity: 1,
+                                        })
+                                    }
+                                    disabled={product.stock === 0 || isLoading}
+                                >
+                                    <HiShoppingCart className="size-4" />
+                                </Button>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
